f6fb5ec753cdc03462d68c36372fb664a9e1bfa5
Alexandru-Paul Copil <alexandru-paul.copil@student.manchester.ac.uk>
2016-11-28 12:20:43 +0000

    Add reset and shift tests. Input from keys.

--- testrun.s
+++ testrun.s
@@ -27,35 +27,31 @@
 ;// RIGHT	LEFT
 ;// --------------------------------------------------------------------
 
-;// DEFINE THE PLAYER POSITIONS ON THE LEFT MOST
-;// 7 SEGEMENT DIGIT DISPLAY.
-
-top	DEFW	&0001	;// Display: 0000_0001
-mid	DEFW	&0002	;// Display: 0100_0000
-bot	DEFW	&0003	;// Display: 0000_1000
-nul	DEFW	&0000	;// Display: 1000_0000
 
 ;// ---------------------------
 ;// PROGRAM RUNTIME BEGINS HERE
 ;// START
 ;// ---------------------------
 
-start	ORG	0000	;// BEGIN AT RESET ADDRESS
-
-	JMP	m_flush
+start	ORG	0000
 
 
-
-
+;// TAKE INPUT FROM THE BOARD
+input
 
 
 
+	LDA	&FF2	;// CHECK ROW4
+	SUB	krs	;// CHECK reset
+	JGE	m_flush	;// FLUSH BOARD
+	
+	LDA	&FF2	;// CHECK ROW4
+	SUB	krh	;// CHECK shift
+	JGE	m_dat	;// SET DIGITS TOP
+	
 
-
-
-
-
-
+	JMP	input
+;// LOOP HERE FOR INPUTS
 
 
 stop	STP
@@ -63,6 +59,24 @@ stop	STP
 ;// PROGRAM RUNTIME ENDS HERE
 ;// STOP
 ;// -------------------------
+
+;// -------------------------
+;// MEMORY ALOCATION
+;// -------------------------
+
+;// DEFINE THE PLAYER POSITIONS ON THE LEFT MOST
+;// 7 SEGEMENT DIGIT DISPLAY.
+;// CAN BE REUSED FOR OTHER PURPOSES TOO
+
+
+top	DEFW	&0001	;// Display: 0000_0001
+mid	DEFW	&0002	;// Display: 0100_0000
+bot	DEFW	&0003	;// Display: 0000_1000
+nul	DEFW	&0000	;// Display: 1000_0000
+
+krs	DEFW	&0080	;// KeyRow4: reset
+krh	DEFW	&0040	;// KeyRow4: shift
+
 
 ;// ----------------------------------
 ;// UTILITY METHODS GO BELOW THIS LINE
@@ -77,6 +91,8 @@ stop	STP
 ;//     m - mid
 ;//     b - bot
 
+
+STP
 m_all	;// SET ALL DISPLAYS TO THE VALUE IN ACC
 	STA	&FF5	;// display 0
 	STA	&FF6	;// display 1
@@ -84,26 +100,31 @@ m_all	;// SET ALL DISPLAYS TO THE VALUE IN ACC
 	STA	&FF8	;// display 3
 	STA	&FF9	;// display 4
 	STA	&FFA	;// display 5
+	
+	JMP	input	;// RESET BACK TO input
 
-
+STP
 m_flush	;// SET ALL DISPLAYS TO nul
 	LDA	nul
 	JMP	m_all
 ;// END m_flush
 
 
+STP
 m_dat	;// SET ALL DISPLAYS TO top
 	LDA	top
 	JMP	m_all
 ;// END m_dat
 
 
+STP
 m_dam	;// SET ALL DISPLAYS TO mid
 	LDA	mid
 	JMP	m_all
 ;// END m_dam
 
 
+STP
 m_dab	;// SET ALL DISPLAYS TO bot
 	LDA	bot
 	JMP	m_all
