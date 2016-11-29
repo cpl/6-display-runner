e00acb4d560fba558e374d7fdf60f4df63b9ac4b
Alexandru-Paul Copil <alexandru-paul.copil@student.manchester.ac.uk>
2016-11-29 12:17:19 +0000

    Clean methods. Clean start, reset, stop.

--- alpha.s
+++ alpha.s
@@ -23,9 +23,30 @@ init	ORG	0000	;// RESET MEMORY ADRESS
 
 			JMP	mrst	;// RESET BOARD COMPONENTS
 
-start						;// START PROGRAM RUNTIME
+runt						;// START PROGRAM RUNTIME
+
+menu						;// LOOP FOR USER START (C)
+			LDA	ir1		;// CHECK INPUT KEYROW 1
+			SUB	ksa		;// CHECK INPUT KEYROW 1 START
+			JNE	menu	;// LOOP menu
+			JGE	load	;// START LOADING TIME
+
+load
 
 
+
+skip						;// IF NO INPUT, DELAY AND SKIP
+
+
+input						;// LOOP FOR USER INPUT
+
+
+
+halt
+			LDA	one		;// SET ACC TO ONE
+			STA stp		;// SET STOP SIGNAL TO ONE
+
+			JMP mrst	;// RESET THEN STOP
 
 ;// 	|-------------------------------------|
 ;// 	| PROGRAM RUNTIME STOPS HERE          |
@@ -35,6 +56,33 @@ start						;// START PROGRAM RUNTIME
 ;// 	| PROGRAM METHODS                     |
 ;//		|-------------------------------------|
 
+STP
+mrst							;// RESET/STOP THE PROGRAM
+			LDA nul			;// LOAD NULL
+
+			STA	dp0			;// RESET DISPLAY 0
+			STA	dp1			;// RESET DISPLAY 1
+			STA	dp2			;// RESET DISPLAY 2
+			STA	dp3			;// RESET DISPLAY 3
+			STA	dp4			;// RESET DISPLAY 4
+			STA	dp5			;// RESET DISPLAY 5
+
+			STA	dbg			;// RESET BAR GRAPH
+
+			LDA	stp			;//	LOAD STOP SIGNAL
+			JNE	runt		;//	START THE PROGRAM
+
+			STP					;// STOP THE PROGRAM
+
+STP
+mlal							;// SET ALL DISPLAYS TO ACC
+			STA	dp0			;// SET DISPLAY 0
+			STA	dp1			;// SET DISPLAY 1
+			STA dp2			;// SET DISPLAY 2
+			STA	dp3			;// SET DISPLAY 3
+			STA	dp4			;// SET DISPLAY 4
+			STA	dp5			;// SET DISPLAY 5
+			STA	dbg			;// SET BAR GRAPH
 
 ;// 	|-------------------------------------|
 ;// 	| PROGRAM MEMORY ALOCATION            |
@@ -55,7 +103,7 @@ dp3		DEFW	&FF8	;// DISPLAY 3
 dp4		DEFW	&FF9	;// DISPLAY 4
 dp5		DEFW	&FFA	;// DISPLAY 5
 
-dgb		DEFW	&FFE	;//	BAR GRAPH
+dbg		DEFW	&FFE	;//	BAR GRAPH
 
 ;//		| BOARD COMPONENTS INPUTS							|
 
@@ -71,24 +119,24 @@ one		DEFW	&0001	;// CONSTANT ONE  VALUE
 
 ;//		| SIGNALS															|
 
-sstp	DEFW	&0000	;// PROGRAM STOP SIGNAL
+stp		DEFW	&0000	;// PROGRAM STOP SIGNAL
 
 ;//		| KEYROWS															|
 
-krst	DEFW	&0080	;// KEYROW 4, RESET
-kstp	DEFW	&0002	;// KEYROW 1, AC
-ksar	DEFW	&0004	;// KEYROW 1, C
-ksft	DEFW	&0040	;// KEYROW 4, SHIFT
+krt		DEFW	&0080	;// KEYROW 4, RESET
+kst		DEFW	&0002	;// KEYROW 1, AC
+ksa		DEFW	&0004	;// KEYROW 1, C
+ksf		DEFW	&0040	;// KEYROW 4, SHIFT
 
 ;//		|	SWITCHES														|
 
-lmov	DEFW	&0001	;// SWITCH 1
-rmov	DEFW	&0002	;//	SWITCH 2
-mmov	DEFW	&0003	;//	SWITCH 1 & SWITCH 2
+lmv		DEFW	&0001	;// SWITCH 1
+rmv		DEFW	&0002	;//	SWITCH 2
+mmv		DEFW	&0003	;//	SWITCH 1 & SWITCH 2
 
 ;//		| DELAYS															|
-dlod	DEFW	&FFFF	;//	MAX LOAD TIME
-dcon	DEFW	&FFF0	;// WAIT FOR F LOOPS
+dld	DEFW	&FFFF	;//	MAX LOAD TIME
+dco	DEFW	&FFF0	;// WAIT FOR F LOOPS
 
 ;//		|-------------------------------------|
 ;//		| SOURCE CODE STOPS HERE              |
