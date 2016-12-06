ac74177fa0f2f788c0fd1430590c97653731b95c
Alexandru-Paul Copil <alexandru-paul.copil@student.manchester.ac.uk>
2016-12-06 10:45:43 +0000

    Update MU0PAL.

--- MU0PAL.py
+++ MU0PAL.py
@@ -1,8 +1,16 @@
+import sys
 
-count = 0
-with open('alpha.s', 'r') as source:
-	for line in source.readlines():
-		if not line.startswith(';') and line.split():
-			print line
-			count = count + 1
-print count
+if __name__ == '__main__':
+    if 'lines' in sys.argv:
+        count = 0
+        with open('alpha.s', 'r') as source:
+            for line in source.readlines():
+                if not line.startswith(';') and line.split():
+                    print line
+                    count = count + 1
+        print count
+    if 'marker' in sys.argv:
+        with open('alpha.s', 'r') as source:
+            for i, line in enumerate(source.readlines()):
+                if line.startswith(';#pythonmark'):
+                    print i, line
