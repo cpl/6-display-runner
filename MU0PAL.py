d765d9d379247c5195f1b6e78690c302eb561f91
Alexandru-Paul Copil <alexandru-paul.copil@student.manchester.ac.uk>
2016-12-07 13:37:21 +0000

    Working MU0PAL sequence generator.

--- MU0PAL.py
+++ MU0PAL.py
@@ -1,55 +1,97 @@
 #!/usr/bin/python
 
+import random
 import sys
 
-OUTPUT = []
+OUTPUT = []  # Store lines from given file and rewrite initial file.
 
-HEADER = ';#py'
-COMMENT = ';'
-END_MARK = HEADER + 'END'
-SEQ_MARK = HEADER + 'SEQ'
-LNK_MARK = HEADER + 'LNK'
+HEADER = ';#py'             # Python marker HEADER
+COMMENT = ';'               # Assembly comment
+END_MARK = HEADER + 'END'   # END any start tag
+SEQ_MARK = HEADER + 'SEQ'   # SEQ start SEQUENCE writing
+LNK_MARK = HEADER + 'LNK'   # LNK start LINKING the SEQUENCE
 
-ENB = False
-MOD = 0
+NEW_LINE = '\n'             # New line char
+
+SEQ_MAX = 6                 # The number of SEQUENCES
+ENB = False                 # Enable write/read lines
+
+POSITIONS = {0: 'top', 1: 'mid', 2: 'bot'}
+
+
+def random_position():
+    return POSITIONS[random.randrange(0, 3)]
+
+
+def make_sequence():
+    for i in range(SEQ_MAX):
+        OUTPUT.append('car' + str(i+1) + NEW_LINE)
+        OUTPUT.append('LDA ' + random_position() + NEW_LINE)
+        OUTPUT.append('STA dp0' + NEW_LINE)
+        OUTPUT.append('JMP nsq' + NEW_LINE)
+
+
+def make_linking():
+    for i in range(SEQ_MAX):
+        OUTPUT.append('JMP car' + str(i+1) + NEW_LINE)
+
 
 if __name__ == '__main__':
 
+    ''' Take any .s file and return a light version with
+        no comments, empty lines, etc...
+    '''
     if 'light' in sys.argv:
         with open('alpha.s', 'r') as source:
             for line in source.readlines():
                 if not line.startswith(COMMENT) and line.split():
                     for j, c in enumerate(line):
-                        if c == ';':
+                        if c == COMMENT:
                             print line[0:j]
                             break
 
+    ''' Return the positions of all Python markers '''
     if 'marker' in sys.argv:
         with open('pytest.s', 'r') as source:
             for i, line in enumerate(source.readlines()):
                 if line.startswith(HEADER):
                     print i, line
 
+    ''' Clear the SEQ and LNK blocks from the given file '''
     if 'clear' in sys.argv:
         with open('pytest.s', 'r') as source:
             for i, line in enumerate(source.readlines()):
                 if line.startswith(END_MARK):
-                    print i, line,
                     ENB = False
-                    MOD = 0
 
                 if not ENB:
                     OUTPUT.append(line)
 
                 if line.startswith(SEQ_MARK):
-                    print i, line,
                     ENB = True
-                    MOD = 1
 
                 if line.startswith(LNK_MARK):
-                    print i, line,
                     ENB = True
-                    MOD = 2
+
+        with open('pytest.s', 'w') as rewrite:
+            rewrite.writelines(OUTPUT)
+
+    if 'sequence' in sys.argv:
+        with open('pytest.s', 'r') as source:
+            for i, line in enumerate(source.readlines()):
+                if line.startswith(END_MARK):
+                    ENB = False
+
+                if not ENB:
+                    OUTPUT.append(line)
+
+                if line.startswith(SEQ_MARK):
+                    ENB = True
+                    make_sequence()
+
+                if line.startswith(LNK_MARK):
+                    ENB = True
+                    make_linking()
 
         with open('pytest.s', 'w') as rewrite:
             rewrite.writelines(OUTPUT)
--- pytest.s
+++ pytest.s
@@ -1,7 +1,39 @@
 ;#pySEQ
-
+car1
+LDA bot
+STA dp0
+JMP nsq
+car2
+LDA bot
+STA dp0
+JMP nsq
+car3
+LDA bot
+STA dp0
+JMP nsq
+car4
+LDA top
+STA dp0
+JMP nsq
+car5
+LDA mid
+STA dp0
+JMP nsq
+car6
+LDA top
+STA dp0
+JMP nsq
 ;#pyEND
 
-;#pyLNK
+LDA stuff ;// THING
+STA TEST
+;// COMMENETS
 
+;#pyLNK
+JMP car1
+JMP car2
+JMP car3
+JMP car4
+JMP car5
+JMP car6
 ;#pyEND
